78d5baeb84fa3e3df9bffe9aac2f6dc5499a2ef2
Gage Norton <nortongage0@gmail.com>
2023-05-15 18:28:32 -0500

    Add boiler code for server

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+const colors = require('colors');
+const { typeDefs, resolvers } = require('./schemas');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+//   context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  connectDB.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer();
