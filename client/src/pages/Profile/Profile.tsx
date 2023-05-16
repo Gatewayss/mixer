a83a3c8c13ddd9bfb0e9545cacffb2c545530ca8
Sarah Richens <sarahgrichens@gmail.com>
2023-05-16 14:07:10 -0500

    add canvas, challenge and navbar pages

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './profile.css';
 
+import Header from '../../components/Header/Header';
 import PostForm from '../../components/PostForm/PostForm';
 import PostList from '../../components/PostList/PostList';
 
 const Profile = () => {
   
-    return (
-     
+    return (     
        <div>
+        <Header />
          This is the profile page
          <PostForm />
          <PostList />
--- client/src/pages/SinglePost/SinglePost.tsx
+++ client/src/pages/SinglePost/SinglePost.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './singlePost.css';
 
+import Header from '../../components/Header/Header';
 import CommentList from '../../components/CommentList/CommentList';
 import CommentForm from '../../components/CommentForm/CommentForm';
+
 
 const SinglePost = () => {
   
     return (
      
        <div>
+        <Header />
          This is a single post
          <CommentList />
          <CommentForm />
