a7395b7550828869fcb5699bbdb41fba5e30b87b
Gage Norton <nortongage0@gmail.com>
2023-05-15 20:49:52 -0500

    Change db name

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,7 @@
 const db = require('../config/db');
 const { User, Post } = require('../models');
-// const userSeeds = require('./userSeeds.json');
-// const postSeeds = require('./postSeeds.json');
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json');
 
 db.once('open', async () => {
   try {
