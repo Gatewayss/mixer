b4ca694f3290bf6a1e3d6ae56bc59086ac5e4bee
Gage Norton <nortongage0@gmail.com>
2023-05-21 22:58:57 -0500

    Add login alerts

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,17 +1,18 @@
 import React, { useState, FormEvent, ChangeEvent } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaintbrushPencil } from '@fortawesome/pro-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaintbrushPencil } from '@fortawesome/pro-regular-svg-icons';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
-import './login.css'
+import './login.css';
 
+import Swal from 'sweetalert2';
 import Auth from '../../utils/auth';
 
 const Login = (props: any) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
-  
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
@@ -20,7 +21,7 @@ const Login = (props: any) => {
       [name]: value,
     });
   };
-  
+
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(formState);
@@ -30,10 +31,22 @@ const Login = (props: any) => {
       });
 
       Auth.login(data.login.token);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Login Successful',
+        text: 'You have been logged in successfully.',
+      });
     } catch (e) {
       console.error(e);
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Login Failed',
+        text: 'An error occurred during login.',
+      });
     }
-  
+
     setFormState({
       email: '',
       password: '',
@@ -42,62 +55,69 @@ const Login = (props: any) => {
 
   return (
     <div className="login-container">
-    <div className="login-card">
-      <div className="login-left">
-        <div className="logo">
-            <h1 className="logo-header">                    
+      <div className="login-card">
+        <div className="login-left">
+          <div className="logo">
+            <h1 className="logo-header">
               <span className="m">m</span>
               <span className="i">i</span>
-              <span className="login-icon"><FontAwesomeIcon icon={faPaintbrushPencil} flip="vertical" /></span>
+              <span className="login-icon">
+                <FontAwesomeIcon icon={faPaintbrushPencil} flip="vertical" />
+              </span>
               <span className="e">e</span>
-              <span className="r">r</span>                   
+              <span className="r">r</span>
             </h1>
-            <p className="logo-tag">Artist block? Mix it up.</p>                 
+            <p className="logo-tag">Artist block? Mix it up.</p>
           </div>
         </div>
-      
-      <div className="login-right">     
-        <div className="login-right-container">  
-          <form className="login-form" onSubmit={handleFormSubmit}>
-            <input
-              className="login-input"
-              placeholder="Email"
-              name="email"
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <input
-              className="login-input"
-              placeholder="Password"
-              name="password"
-              type="password"
-              value={formState.password}
-              onChange={handleChange}
-            />
-            <button
-              className="login-btn"
-              style={{ cursor: 'pointer' }}
-              type="submit"
-            >
-              Login
-            </button>
-          </form>    
-          {error && (
-            <div>
-              {error.message}
+
+        <div className="login-right">
+          <div className="login-right-container">
+            <form className="login-form" onSubmit={handleFormSubmit}>
+              <input
+                className="login-input"
+                placeholder="Email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="login-input"
+                placeholder="Password"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button
+                className="login-btn"
+                style={{ cursor: 'pointer' }}
+                type="submit"
+                onClick={() => {
+                  Swal.fire({
+                    title: 'Logging In...',
+                    allowOutsideClick: false,
+                    didOpen: () => {
+                      Swal.showLoading();
+                    },
+                  });
+                }}
+              >
+                Login
+              </button>
+            </form>
+            {error && <div>{error.message}</div>}
+            <div className="no-account">
+              <p className="no-acct-tag">Don't have an account?</p>
+              <Link className="signup-link" to="/signup">
+                <button className="go-signup-btn">Sign up</button>
+              </Link>
             </div>
-          )}         
-          <div className="no-account">
-            <p className="no-acct-tag">Don't have an account?</p>
-            <Link className="signup-link" to="/signup">
-              <button className="go-signup-btn" >Sign up</button>
-            </Link>
           </div>
-        </div>  
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
