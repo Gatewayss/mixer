690d8a043b8b8f5c4314dbed68808985f1aa9f7f
Gage Norton <nortongage0@gmail.com>
2023-05-21 23:16:22 -0500

    Add counter

--- client/src/pages/Challenge/Challenge.js
+++ client/src/pages/Challenge/Challenge.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import './challenge.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CHALLENGE } from '../../utils/queries';
-
 import Header from '../../components/Header/Header';
 
 const Challenge = () => {
   const { loading, data } = useQuery(QUERY_CHALLENGE);
-
-  const challenges = data?.challenge|| [];
+  const challenges = data?.challenge || [];
   const [currentChallengeIndex, setCurrentChallengeIndex] = useState(0);
+  const [countdown, setCountdown] = useState(() => {
+    const storedCountdown = parseInt(localStorage.getItem('countdown'), 10);
+    return !isNaN(storedCountdown) && storedCountdown > 0 ? storedCountdown : 24 * 60 * 60;
+  });
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentChallengeIndex(prevIndex => (prevIndex + 1) % challenges.length);
-    }, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
+      setCountdown(24 * 60 * 60); // Reset the countdown to 24 hours
+    }, 24 * 60 * 60 * 1000); // 24 hours in seconds
 
     return () => clearInterval(interval);
   }, [challenges.length]);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown(prevCountdown => {
+        const newCountdown = prevCountdown - 1;
+        localStorage.setItem('countdown', newCountdown.toString());
+        return newCountdown;
+      });
+    }, 1000); 
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('countdown', countdown.toString());
+  }, [countdown]);
+
   const currentChallenge = challenges[currentChallengeIndex];
+
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
   return (
     <div className="challenge-container">
-    <Header />
-    {currentChallenge && (
-      <div key={currentChallenge.id}>
-        <p>{currentChallenge.challengeTitle}: </p>
-        <p>{currentChallenge.challengeDescription}</p>
-      </div>
-    )}
-  </div> 
-);
+      <Header />
+      {currentChallenge && (
+        <div key={currentChallenge.id}>
+          <p>{currentChallenge.challengeTitle}: </p>
+          <p>{currentChallenge.challengeDescription}</p>
+        </div>
+      )}
+      <p>Time Left: {formatTime(countdown)}</p>
+    </div>
+  );
 };
 
 export default Challenge;
