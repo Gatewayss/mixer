72298965ebe9013827694b921b5e25375da11ab9
Gage Norton <nortongage0@gmail.com>
2024-04-22 14:30:33 -0500

    Deploy

--- server/index.js
+++ server/index.js
@@ -30,14 +30,15 @@ app.get('/*', (req, res) => {
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-  };
-  
+};
+
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
+
