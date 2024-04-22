5c7a2c74a73e21c63f4e02044714e07a68a6dcad
Gage Norton <nortongage0@gmail.com>
2024-04-22 13:23:01 -0500

    Deploy

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,17 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config()
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mixer');
+
+// module.exports = mongoose.connection;
+
 const mongoose = require('mongoose');
-require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mixer');
-
-module.exports = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
